abe2337838083778c96067dd7f3f261b8ce66360
zodako <33731077+zodako@users.noreply.github.com>
2020-10-04 23:23:04 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "96568eaf2345e6bba84024733f1515bf",
 "/": "96568eaf2345e6bba84024733f1515bf",
-"main.dart.js": "31209c43d1b0c784ff89cd1e6767980a",
+"main.dart.js": "eda2e793b95efe1d7c6f75b20eb4dd08",
 "manifest.json": "18ad0294f96b0755889b2b8be73a551f"
 };
 
